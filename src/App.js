2912f5286d640474f11f75740c48ec5ce73aa767
MarCalmon <marical2222@gmail.com>
2018-02-20 11:43:37 -0300

    cambios en App.js y FileUpload.js, uso de Realtime Database en Firebase

--- src/App.js
+++ src/App.js
@@ -9,16 +9,24 @@ class App extends Component {
   constructor(){
     super();
     this.state = {
-      user: null
+      user: null,
+      pictures: []
     };
 
     this.handleAuth = this.handleAuth.bind(this);
     this.handleLogout = this.handleLogout.bind(this);
+    this.handleUpload = this.handleUpload.bind(this);
   }
 
   componentWillMount() {
     firebase.auth().onAuthStateChanged(user => {
       this.setState ({ user });
+    });
+
+    firebase.database().ref('pictures').on('child_added', snapshot => {
+      this.setState({
+        pictures: this.state.pictures.concat(snapshot.val())
+      });
     });
   }
 
@@ -36,6 +44,31 @@ class App extends Component {
       .catch(error => console.log(`Error ${error.code}: ${error.message}`));
   }
 
+  handleUpload(event) {
+		const file = event.target.files[0];
+		const storageRef = firebase.storage().ref(`/fotos/${file.name}`);
+		const task = storageRef.put(file);
+
+		task.on('state_changed', snapshot => {
+			let percentage = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+			this.setState({
+				uploadValue: percentage
+			})
+		}, error => {
+			console.log(error.message)
+		},() => {
+			const record = {
+        photoURL: this.state.user.photoURL,
+        displayName: this.state.user.displayName,
+        image: task.snapshot.downloadURL
+      };
+
+      const dbRef = firebase.database().ref('pictures');
+      const newPicture = dbRef.push();
+      newPicture.set(record);
+		});
+	}
+
   renderLoginButton(){
     //si el usuario está logueado
     if(this.state.user) {
@@ -44,7 +77,21 @@ class App extends Component {
           <img width="100" src={this.state.user.photoURL} alt={this.state.user.displayName} />
           <p>Hola {this.state.user.displayName}!</p>
           <button onClick={this.handleLogout}>Salir</button>
-          <FileUpload />
+          
+          <FileUpload onUpload={ this.handleUpload }/>
+
+          {
+            this.state.pictures.map(picture => (
+              <div>
+                <img src={ picture.image } />
+                <br/>
+                <img width="100" src={ picture.photoURL } alt={picture.displayName} />
+                <br/>
+                <span>{picture.displayName}</span>
+              </div>
+            )).reverse()
+          }
+
         </div>
       );
     }else { //si no lo está
